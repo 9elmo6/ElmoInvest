61cb2b7de62ae5c15139afd8e9f14d30666ceb54
elmo <63063385+9elmo6@users.noreply.github.com>
2023-02-09 10:29:46 -0500

    Add files via upload

--- MTEchoServer.java
+++ MTEchoServer.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.util.*;
 import java.io.*;
  
 public class MTEchoServer {
@@ -21,20 +22,33 @@ private static class Stock {
             return price;
         }
     }
-        Stock google = new Stock("GOOG", 1234.56);
-        Stock amazon = new Stock("AMZN", 2345.67);
-        Stock apple = new Stock("AAPL", 3456.78);
+    private List<Stock> stocks = new ArrayList<>();
+    static HashMap<String, String> users = new HashMap<>();
 
-
-
-
+    private void readStocksFromFile(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length >= 2) {
+                    String name = parts[0];
+                    double price = Double.parseDouble(parts[1]);
+                    Stock stock = new Stock(name, price);
+                    stocks.add(stock);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error while reading stocks from file: " + e.getMessage());
+        }
+    }
+    
 
     public static void main(String[] args) {
-         
-      
-         
         int portNumber =8080;
+        users.put("user1", "password1");
+        users.put("admin", "password2");
         MTEchoServer es = new MTEchoServer();
+        es.readStocksFromFile("stocks.txt");
         es.run(portNumber);
      }
      public void run(int portNumber) {
@@ -51,23 +65,18 @@ client.getRemoteSocketAddress() );
            System.out.println("Exception: "+e);
         }
     }
-         
-  
 
-class Connection extends Thread {
+/*class Connection extends Thread {
     Socket client;
-    //PrintWriter out;
-    //BufferedReader in;
-     ObjectOutputStream out;
-      ObjectInputStream in;
+    PrintWriter out;
+    BufferedReader in;
     public Connection(Socket s) { // constructor
        client = s;
-          
+       
        try {
-          out = new ObjectOutputStream(client.getOutputStream());
-            in = new ObjectInputStream(client.getInputStream());
-          // out = new PrintWriter(client.getOutputStream(), true);                  
-           //in = new BufferedReader(   new InputStreamReader(client.getInputStream()));
+           out = new PrintWriter(client.getOutputStream(), true);                  
+           in = new BufferedReader(
+                new InputStreamReader(client.getInputStream()));
        } catch (IOException e) {
            try {
              client.close();
@@ -77,41 +86,131 @@ class Connection extends Thread {
            return;
        }
         this.start(); // Thread starts here...this start() will call run()
+    }
+  */
+class Connection extends Thread {
+    Socket client;
+    PrintWriter out;
+    BufferedReader in;
+    public Connection(Socket s) { // constructor
+       client = s;
+       try {
+        out = new PrintWriter(client.getOutputStream(), true);                  
+        in = new BufferedReader(
+             new InputStreamReader(client.getInputStream()));
+       } catch (IOException e) {
+           try {
+             client.close();
+           } catch (IOException ex) {
+             System.out.println("Error while getting socket streams.."+ex);
+           }
+           return;
+       }
+        this.start(); 
         
     }
  
     public void run() {
       try {
+                String username = in.readLine();
+                System.out.println(username);
+                String password = in.readLine();
+                System.out.println(password);
+                //System.out.println("read credentials");
+            if (authenticate(username, password)) {
+                //out.println("AUTHENTICATE SUCCESS");
+                System.out.println("in the loop");
+                
 
-            while (true) {
-           
+                int choice= Integer.parseInt(in.readLine()); //read choice
 
-                    String stockName = in.readUTF();
-                    if(stockName.equalsIgnoreCase("EXIT"))
-                    break;
-
-
-
+                switch(choice){
+                    //1. searching of stock price
+                    case  1:
+                    System.out.println("I am in case 1");
+                    String stockName = in.readLine();
                     Stock stock = null;
-                    if (stockName.equals("GOOG")) {
-                        stock = google;
-                    } else if (stockName.equals("AMZN")) {
-                        stock = amazon;
-                    } else if (stockName.equals("AAPL")) {
-                        stock = apple;
+                    for (Stock s : stocks) {
+                        
+                        if (s.getName().equals(stockName)) {
+                            stock = s;
+                            break;
                     }
-
+                }
+                    
                     if (stock != null) {
-                        out.writeDouble(stock.getPrice());
+                        System.out.println("I am in final condition");
+                        System.out.println(stock.getPrice());
+                        out.println(stock.getPrice());
                         out.flush();
                     }
+                    break;
+                    case 2:
+                    //2. Calculate investment
+                    System.out.println("I am in case 2");
+                    String invest= in.readLine();
+                    int nstocks= Integer.parseInt(in.readLine());
+                    stock = null;
+                    System.out.println(invest);
+                    System.out.println(nstocks);
+                    for (Stock s : stocks) {
+                        System.out.println("I am in loop");
+                        if (s.getName().equals(invest)) {
+                            stock = s;
+                            break;
+                    }
+                }
+                    
+                    if (stock != null) {
+                        System.out.println("I am in final condition");
+                        Long investment = (long) (nstocks*stock.getPrice());
+                        out.println(investment);
+                        out.flush();
+                        System.out.println(investment);
+                    
+
+                    }
+                    
+                
+                    
+                    break;
+                    case 3:
+                    //3. Update stock prices (admin only)
+                    System.out.println("I am in case 3");
+                    if (username.equals("admin") ){
+                        //add the code to add or delete stockes file
+                        System.out.println("I am the admin");
+                        String updateName = in.readLine();
+                        Double updatePrice = Double.parseDouble(in.readLine());
+                        Stock stockupdate = new Stock(updateName, updatePrice);
+                    
+                        stocks.add(stockupdate);
+                        out.println("New stock added successfully");
+
+                        for (Stock s : stocks) {
+                            System.out.println("Stock " + s.name);
+                            System.out.println("Price " + s.price);
+                            
+                        }
+                       
+                    }
+                    else out.println("you are not admin");
+
+                    break;
+                    
+                    case 4:
+                    //Exit
+
+                    break;
+                    }
+                    
+                    }
+                else{
+                    out.println("FAIL");
+                    out.println("username or password invalid !!");
 
                 }
             
-
-
-
-        
          client.close();
        } catch (IOException e) {
            System.out.println("Exception caught...");
@@ -119,4 +218,9 @@ class Connection extends Thread {
        }
     }
 }
+
+private boolean authenticate(String username, String password) {
+    return users.containsKey(username) && users.get(username).equals(password);
+}
+
 }
--- StockMarketClient.java
+++ StockMarketClient.java
@@ -1,24 +1,87 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class StockMarketClient {
-    public static void main(String[] args) throws IOException {
-        try (Socket socket = new Socket("localhost", 8080);
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+       /*  try (Socket socket = new Socket("localhost", 8080);
              ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
              ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
-
+*/
             Scanner scanner = new Scanner(System.in);
-            while (true) {
-                System.out.print("Enter stock name: ");
-                String stockName = scanner.nextLine();
-                out.writeUTF(stockName);
-                out.flush();
-                double price = in.readDouble();
-                System.out.println("Price: " + price);
+            try {
+                Socket echoSocket = new Socket("localhost", 8080);
+                PrintWriter out =
+                    new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in =
+                    new BufferedReader(
+                        new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn =
+                    new BufferedReader(
+                        new InputStreamReader(System.in));
+            
+            
+                //Auth with server admin/password2 or user/password1
+                System.out.print("Enter username: ");
+                String username = scanner.nextLine();
+                out.println(username);
+                System.out.print("Enter password: ");
+                String password = scanner.nextLine();
+                out.println(password);
+
+                
+                    //Taking user choice
+                System.out.println("Please choose from the following options");
+                System.out.println("1 Search for a stock");
+                System.out.println("2. Buy stocks");
+                System.out.println("3. Edit stocks (admins only)");
+                System.out.println("4. Exit");
+                String choice = scanner.nextLine();
+                out.println(choice);
+
+                switch(Integer.parseInt(choice)){
+                    case 1:
+                   
+                        System.out.print("Enter stock name: ");
+                        String stockName = scanner.nextLine();
+                        out.println(stockName);
+                        out.flush();
+                        String price = in.readLine();
+                        System.out.println("Price: " + price);
+                    break;
+                    case 2:
+                        System.out.println("Which stock would you like to invest?");
+                        String invest = scanner.nextLine();
+                        out.println(invest); 
+                        System.out.println("How many stocks would you like to buy?");
+                        String nstocks = scanner.nextLine();
+                        out.println(nstocks);
+                        System.out.print("Your investment = "+ in.readLine());    
+                    break;
+                    case 3:
+                    System.out.print("Please enter the stock you want to add ");
+                    String NewStock = scanner.nextLine();
+                    out.println(NewStock);
+                    System.out.print("Please enter the stock price ");
+                    String NewPrice = scanner.nextLine();
+                    out.println(NewPrice);
+                    System.out.println(in.readLine());
+
+                    break;
+                }
+            
+                
+            } catch (UnknownHostException e) {
+                System.err.println("Don't know about host " + "localhost");
+                System.exit(1);
+            } catch (IOException e) {
+                System.err.println("Couldn't get I/O for the connection to " +
+                    "localhost");
+                System.exit(1);
+                
             }
         }
     }
-}
+
+
+
